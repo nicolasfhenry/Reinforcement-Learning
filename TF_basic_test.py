4d126d4e43413facdaa3778c10acab2d70972b90
Vassily <v.carantino@gmail.com>
2017-10-21 18:52:21 -0400

    Add files via upload
    New Version, with Neural Net class, and function RunNN, and GetRewardEpisode (new name for EpisodeRoute, but does the same thing)

--- TF_basic_test.py
+++ TF_basic_test.py
@@ -5,32 +5,105 @@ https://stackoverflow.com/questions/43765841/training-a-tensorflow-neural-networ
 
 @author: vassily
 """
-
+### Libraries ###
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-def initGym():
-    env=gym.make('MountainCar-v0')
-    obs_dim = env.observation_space.shape[0]
-    act_dim = env.action_space.n
-    
-    return env, obs_dim , act_dim
+### Parameters ###
 
-
-env, num_obs, num_action=initGym()
-num_episodes=20
-reward_episode=[]
+gameName='MountainCar-v0'
 alpha =0.01 #parameter gradient
 sigma=0.01 #parameter noise -update Fi
 num_workers=200
-
-#Initialization of the neural net for the game
-numInput=num_obs 
-numOutput=num_action
+num_episodes=20
 numHidden=8 # 8 neurons per Hidden layer
 
+RANDOM_SEED = 42
+tf.set_random_seed(RANDOM_SEED)
+
+### Class Neural Network ###
+
+class NeuralNetwork(object):
+    def __init__(self, input_size, hidden_size, output_size):
+        """
+        :param input_size: number of input neurons
+        :param hidden_size: number of hidden neurons
+        :param output_size: number of output neurons
+        """        
+        
+        self.input_size = input_size 
+        self.hidden_size = hidden_size
+        self.output_size = output_size       
+       
+         # Symbols
+        self.X = tf.placeholder("float", shape=[self.input_size])
+            
+        # Weight initializations
+        self.wi = tf.random_normal((self.input_size,self.hidden_size), stddev=0.1, dtype=tf.float32)
+        self.wo = tf.random_normal((self.hidden_size,self.output_size), stddev=0.1, dtype=tf.float32)   
+        
+    
+    def feedForward(self):
+        self.wh = tf.nn.sigmoid(tf.matmul(tf.expand_dims(self.X, 1), self.wi, transpose_a=True))  # The \sigma function
+        self.output = tf.matmul(self.wh, self.wo)  # The \varphi function
+        self.predict = tf.argmax(self.output, axis=1)
+        return(self.predict)
+
+#Initialise OpenAI gym environment
+def initGym():
+    env=gym.make(gameName)
+    obs_dim = env.observation_space.shape[0]
+    act_dim = env.action_space.n    
+    return env, obs_dim , act_dim
+
+
+env, numInput, numOutput=initGym()
+reward_episode=[]
+initial_observation = env.reset()
+observation=initial_observation
+
+
+# Create NN object:
+NN=NeuralNetwork(numInput,numHidden,numOutput)
+
+
+def runNN(rnn, env):
+    action=rnn.feedForward()  
+    observation = env.reset()    
+    with tf.Session():
+        for step in range(100):
+            env.render()
+            evalAction=action.eval(feed_dict={NN.X: observation})[0]
+            observation, reward, done, info = env.step(evalAction)
+            print(observation)  
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
+
+#runNN(NN,env)
+
+
+def getRewardEpisode(rnn, env, NbSteps=1000):
+    action=rnn.feedForward()  
+    observation = env.reset()
+    cum_reward = 0.0
+    with tf.Session():
+        for j in range(NbSteps):
+            evalAction=action.eval(feed_dict={rnn.X: observation})[0]
+            observation, reward, done, info = env.step(evalAction)
+            if done:
+                break
+            cum_reward += reward        
+        return cum_reward
+
+
+NN=NeuralNetwork(numInput,numHidden,numOutput)
+getRewardEpisode(NN,env,1000)
+
+
+### OLD >>
 def NN(myInput,numInput,numHidden,numOutput):  
     
     wi = tf.random_normal((numInput,numHidden), stddev=0.1, dtype=tf.float32)
@@ -39,6 +112,9 @@ def NN(myInput,numInput,numHidden,numOutput):
     output = tf.matmul(h, wo)  # The \varphi function
     action = tf.argmax(output, axis=1)    
     return(action)
+    
+    
+
     
 
 initial_observation = env.reset()
