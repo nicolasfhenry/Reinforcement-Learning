f143780f7ec579944de7f0f41e6663da8ef70897
nicolash <nicolas.henry.dollet@gmail.com>
2017-12-12 20:47:46 -0500

    update

--- Results/script_auto.py
+++ Results/script_auto.py
@@ -12,31 +12,31 @@ import zipfile
 print( '*' * 50)
 print( '*' * 50)
 
-print('1/5')
+print('1/7')
 os.system('python mainGS_mirror_noVBN.py')
 
 print( '*' * 50)
 print( '*' * 50)
 
-print('2/5')
+print('2/7')
 os.system('python mainGS_mirror_VBN.py')
 
 
 print( '*' * 50)
 print( '*' * 50)
 
-print('3/5')
+print('3/7')
 
 os.system('python mainGS_noMirror_VBN.py')
 
-print('4/5')
+print('4/7')
 
 print( '*' * 50)
 print( '*' * 50)
 
 os.system('python mainGS_noVBN.py')
 
-print('5/5')
+print('5/7')
 
 print( '*' * 50)
 print( '*' * 50)
@@ -63,13 +63,13 @@ print( '*' * 50)
 
 #ZipFile:
 zipf = zipfile.ZipFile('Download.zip', 'w', zipfile.ZIP_DEFLATED)
-zipf.write('mainGS_mirror_noVBN.py.pkl')
-zipf.write('mainGS_mirror_VBN.py.pkl')
-zipf.write('mainGS_noMirror_VBN.py.pkl')
-zipf.write('mainGS_noVBN.py.pkl')
-zipf.write('mainGS_noVBN_noFitness.py.pkl')
-zipf.write('main_noGS_noVBN.py.pkl')
-zipf.write('main_noGS_VBN.py.pkl')
+zipf.write('mainGS_mirror_noVBN.py.pkl') #1
+zipf.write('mainGS_mirror_VBN.py.pkl') #2
+zipf.write('mainGS_noMirror_VBN.py.pkl') #3
+zipf.write('mainGS_noVBN.py.pkl') #4
+zipf.write('mainGS_noVBN_noFitness.py.pkl') #5
+zipf.write('main_noGS_noVBN.py.pkl') #6
+zipf.write('main_noGS_VBN.py.pkl') #7
 
 zipf.close()
 
