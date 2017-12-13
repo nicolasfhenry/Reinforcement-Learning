e8d6b994a29643801ded75a13ae921497318e228
MakhtarBa <bamouhamadou001@gmail.com>
2017-12-12 21:08:47 -0500

    Update main_FS.py

--- main_FS.py
+++ main_FS.py
@@ -273,7 +273,7 @@ def computeVBN(seeds_VBN, params):
 
 if __name__ == "__main__":
     #General parameters
-    params = [np.random.randn(numHidden1,numInput), np.random.randn(numHidden2,numHidden1),np.random.randn(numOutput,numHidden2)]  
+    params = [0.01*np.random.randn(numHidden1,numInput), 0.01*np.random.randn(numHidden2,numHidden1),0.01*np.random.randn(numOutput,numHidden2)]  
     
     num_VBN_workers=20
     seeds_VBN = np.random.randint(10000,size=num_VBN_workers)
@@ -330,7 +330,8 @@ if __name__ == "__main__":
         #epsilons=GS_epsilons_ini+GS_epsilons_neg
         epsilons=GS_epsilons_ini        
         
-        seeds = np.random.randint(10000,size=num_workers)
+        #seeds = np.random.randint(10000,size=num_workers)
+        seeds=np.zeros(num_workers)
 
 
         reward_workers,epsilon_W1,epsilon_W2,epsilon_W3 =  [list(x) for x in  zip(*main(seeds,epsilons,params))]
